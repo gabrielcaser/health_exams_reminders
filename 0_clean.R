79220125f40f9a889914cba3b8d5b998510ec970
gabrielcaser <gabrielcaser9@hotmail.com>
2025-07-12 11:02:51 -0300

    vars and labels

--- 0_clean.R
+++ 0_clean.R
@@ -1,13 +1,20 @@
 # This program cleans the dataset
 
+# TODO
+## criar IDs mesclando os nomes
+
 # Setting the ambience
+rm(list = ls())     # Remove todos objetos
+graphics.off()      # Fecha todas as janelas gráficas
+cat("\014")         # Limpa console (RStudio)
+gc()                # Garbage collection (libera memória)
 
 setwd("C:/Users/gabri/Documents/Github/health_exams_reminders")
 
 # Common used packages
 library(data.table)
 library(skimr)
-library()
+library(labelled)
 
 # Opening dataset
 dt <- fread(
@@ -35,37 +42,47 @@ setnames(
 dt_c <- dt[, n_exam := seq_len(.N), by = .(cns, diagendou)] # REMOVER DEPOIS
 
 # Deidentifing
-dt_c[, pacient_id := as.character(as.integer(factor(cns)))]
-dt_c[, nome := NULL]
-dt_c[, cns := NULL]
-dt_c[, exam_id := as.character(.I)]
+dt_c <- dt_c[, pacient_id := as.character(as.integer(factor(cns)))]
+dt_c <- dt_c[, nome := NULL]
+dt_c <- dt_c[, cns := NULL]
+dt_c <- dt_c[order(diagendou, n_exam)]
+dt_c <- dt_c[, pde_id := as.character(.I)]
 
 # Droping duplicates
 dt_duplicatet <- dt_c[duplicated(dt_c), ]
 dt_c <- dt_c[!duplicated(dt_c), ]
 
 # Tidying
+
 ## pacient level
 dt_p <- dt_c[, .(pacient_id, raca, genero)]
 dt_p <- dt_p[!duplicated(dt_p), ]
 
-## exam level
-dt_e <- dt_c[, .(exam_id, n_exam, pacient_id, situacao, unidade, diagendou, diaserafeito)]
+## pacient-day-exam level
+dt_pde <- dt_c[, .(pde_id, n_exam, pacient_id, situacao, unidade, diagendou, diaserafeito)]
+setcolorder(dt_pde, c("pde_id", "pacient_id", "diagendou", "n_exam"))
+
+# Changing types
+dt_pde[, unidade := factor(unidade)]
+dt_pde[, diagendou := as.Date(diagendou, format = "%d/%m/%Y")]
+dt_pde[, diaserafeito := as.Date(diaserafeito, format = "%d/%m/%Y")]
+
+# Labeling
 
 # For pacient-level dataset
-labelled::var_label(dt_p) <- list(
+var_label(dt_p) <- list(
   pacient_id = "Anonymized patient ID",
   raca       = "Race",
   genero     = "Gender"
 )
 
 # For exam-level dataset
-labelled::var_label(dt_e) <- list(
-  exam_id        = "Unique exam ID",
+var_label(dt_pde) <- list(
+  pde_id         = "Unique patient-day-exam ID",
   n_exam         = "Exam name",
   pacient_id     = "Anonymized patient ID",
   situacao       = "Exam status",
-  unidade        = "Health unit name",
+  unidade        = "Health unit scheduled",
   diagendou      = "Date when exam was scheduled",
   diaserafeito   = "Date when exam was performed"
 )
@@ -74,5 +91,5 @@ labelled::var_label(dt_e) <- list(
 rm(dt, dt_c, dt_duplicatet)
 
 # Saving clean dataset
-saveRDS(dt_p, "data/intermediary/clean_data_pacient.rds")
-saveRDS(dt_e, "data/intermediary/clean_data_exam.rds")
+saveRDS(dt_p,   "data/intermediary/clean_data_pacient.rds")
+saveRDS(dt_pde, "data/intermediary/clean_data_pacient-day-exam.rds")
--- 1_construct.R
+++ 1_construct.R
@@ -1,33 +1,80 @@
 # This program construct the dataset for analysis
 
 # Opening clean datasets
-dt_p <- as.data.table(readRDS("data/intermediary/clean_data_pacient.rds"))
-dt_e <- as.data.table(readRDS("data/intermediary/clean_data_exam.rds"))
+dt_p   <- as.data.table(readRDS("data/intermediary/clean_data_pacient.rds"))
+dt_pde <- as.data.table(readRDS("data/intermediary/clean_data_pacient-day-exam.rds"))
 
 # Creating variables
-
-## dummies
-dt_p[, homem := fcase(genero == "MASCULINO", 1L,
+dt_p <- dt_p[, homem := fcase(genero == "MASCULINO", 1L,
                       genero == "FEMININO", 0L,
                       default = NA_integer_)]
 
-dt_e[, dias_espera := diaserafeito - diagendou]
+dt_pde <- dt_pde[, dias_espera := as.integer(diaserafeito - diagendou)]
 
-dt_e[, compareceu := fcase(situacao == "Realizou", 1L,
+dt_pde <- dt_pde[, dia_feito_semana := format(as.Date(diaserafeito), "%a")]
+dt_pde <- dt_pde[, dia_feito_semana := factor(
+  dia_feito_semana,
+  levels = c("seg", "ter", "qua", "qui", "sex", "sáb", "dom"),
+  ordered = TRUE
+)]
+
+dt_pde <- dt_pde[, compareceu := fcase(situacao == "Realizou", 1L,
                            situacao == "Faltou", 0L,
                            default = NA_integer_)]
-## number of faltas
-dt_e[, .(n_faltas = sum(compareceu)), by = pacient_id][order(-n_faltas)]
-dt_e[, .(n_obs = .N), by = pacient_id][order(-n_obs)]
 
-dt_e[, n_faltas := sum(compareceu), by = pacient_id]
-dt_e[, n_obs := .N, by = pacient_id]
-dt_e[, perc_faltas := n_faltas / n_obs]
+# Reshaping to Pacient-Day level
+
+dt_pd <- dt_pde[, .(
+  n_exames = .N,
+  n_compareceu = sum(compareceu)
+), by = .(pacient_id, diagendou)][order(diagendou)]
+
+dt_pd <- dt_pd[, mean_compareceu := n_compareceu / n_exames]
+
+dt_pd <- dt_pd[, pd_id := as.character(.I)]
+
+setcolorder(dt_pd, c("pd_id", "pacient_id"))
 
 # Removing variables
 dt_p[, genero := NULL]
-dt_e[, situacao := NULL]
+dt_pde[, situacao := NULL]
+
+# Changing types
+dt_pde[, compareceu := factor(compareceu, levels = c(0, 1), labels = c("No", "Yes"))]
+dt_p[, homem := factor(homem, levels = c(0, 1), labels = c("No", "Yes"))]
+
+# Labels for dt_p
+var_label(dt_p$pacient_id) <- "Anonymized patient identifier"
+var_label(dt_p$raca)       <- "Patient race/color"
+var_label(dt_p$homem)      <- "Male indicator"
+
+# Labels for dt_pde
+var_label(dt_pde$compareceu)       <- "Attendance indicator"
+var_label(dt_pde$dias_espera)      <- "Days between scheduling and performance"
+var_label(dt_pde$dia_feito_semana) <- "Day of the week to attend"
+
+# Labels for dt_pd
+var_label(dt_pd$pd_id)           <- "Unique patient-day identifier"
+var_label(dt_pd$n_exames)        <- "Number of exams scheduled on the day"
+var_label(dt_pd$n_compareceu)    <- "Number of attended exams on the day"
+var_label(dt_pd$mean_compareceu) <- "Proportion of exams attended on the day"
+
+# Changing columns order
+setcolorder(dt_pde, c(
+  "pde_id",
+  "pacient_id",
+  "n_exam",
+  "diagendou",
+  "diaserafeito",
+  "dia_feito_semana",
+  "dias_espera",
+  "unidade",
+  "compareceu"
+))
+
+# Save cleaned datasets
+saveRDS(dt_p,  "data/intermediary/constructed_data_pacient.rds")
+saveRDS(dt_pd, "data/intermediary/constructed_data_pacient-day.rds")
+saveRDS(dt_pde,"data/intermediary/constructed_data_pacient-day-exam.rds")
 
 
-
-# Reshaping from exam-level to person-level
